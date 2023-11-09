59e037be64c113702484bffff6312eacd06c17ce
Enterprise-J <sunjiachen-braxton@outlook.com>
2023-11-08 21:00:22 -0500

    Initial commit

---
+++ 0_processing_per_cell.py
@@ -0,0 +1,91 @@
+import os
+import pandas as pd
+import numpy as np
+import pygini
+import sys
+
+# %%
+cell_dir = sys.argv[1]
+script_dir = sys.argv[2]
+path_linear_model = sys.argv[3]
+cnv_name = sys.argv[4]
+mat_name = sys.argv[5]
+input_dir = sys.argv[6]
+# %%
+
+cell_name = cell_dir.split('/')[-1]
+sample_name = input_dir.split('/')[-1]
+
+path_bed_graph = cell_dir + "/" + cnv_name
+path_contact_matrix = cell_dir + "/" + mat_name
+output_directory = script_dir + "/" + "ecDNA_prediction_" + sample_name
+output_file_path = output_directory + '/' + cell_name + '.txt'
+
+if (not os.path.exists(path_bed_graph)) or (not os.path.exists(path_contact_matrix)):
+    print(cell_name + ": one or more file(s) not exist.")
+    exit(1)
+
+# %%
+res = pd.read_table(path_bed_graph, header=None)
+mat = pd.read_table(path_contact_matrix)
+
+if len(res) == 0 or len(mat) == 0:
+    print(cell_name + ": one or more file(s) not valid.")
+    exit(2)
+# %%
+num_chr = 23
+name_chr = ['chr' + str(i) for i in range(1, 23)] + ['chrX']
+
+res.columns = ['chr', 'start', 'end', 'CNV']
+
+res['num.intra.bin'] = 0
+res['num.inter.bin'] = 0
+# %%
+rec = pd.DataFrame(np.zeros((len(res), num_chr), dtype=int), columns=name_chr)
+# %%
+for j in range(len(res)):
+    overlap = mat[
+        (mat['chrom1'] == res['chr'][j]) & (mat['start1'] == res['start'][j]) | (mat['chrom2'] == res['chr'][j]) & (
+                mat['start2'] == res['start'][j])]
+    intra = overlap[(overlap['chrom1'] == overlap['chrom2']) & (overlap['start1'] == res['start'][j])]
+    inter = overlap[(overlap['chrom1'] != overlap['chrom2']) & (overlap['chrom1'] == res['chr'][j])]
+
+    inter = inter[(inter['chrom1'] != 'chrY') & (inter['chrom2'] != 'chrY')]
+
+    res.at[j, 'num.intra.bin'] = len(intra)
+    res.at[j, 'num.inter.bin'] = len(inter)
+
+    for k in range(num_chr):
+        rec.at[j, name_chr[k]] = np.sum(inter['chrom2'] == name_chr[k])
+
+
+# %%
+res['gini'] = np.nan
+
+for j in range(len(res)):
+    gbin = np.array(rec.iloc[j, :])
+    if np.sum(gbin) > 0:
+        gini_coefficient = pygini.gini(gbin.astype("float64"))  # Calculate Gini coefficient
+        res.at[j, 'gini'] = gini_coefficient
+# %%
+res['inter.intra.log2ratio'] = round(np.log2(res['num.inter.bin'] + 1) - np.log2(res['num.intra.bin'] + 1), 4)
+# %%
+linear_model = pd.read_csv(path_linear_model, sep='\t', header=0)
+# %%
+input = res.iloc[:, [0, 1, 2, 3, 7, 6]]
+input.columns = ['chr', 'start', 'end', 'cnv', 'ratio', 'gini']
+input.loc[:, 'cnv'] = input['cnv'].apply(lambda value: min(value, 100))
+# %%
+res['eta'] = linear_model['Estimate'][0] + linear_model['Estimate'][1] * input['cnv'] + linear_model['Estimate'][2] * \
+             input['ratio'] + linear_model['Estimate'][3] * input['gini']
+res['pred'] = 1 / (1 + np.exp(-res['eta']))
+
+res.loc[res['gini'].isna(), 'pred'] = 0
+
+# %%
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+res.to_csv(output_file_path, sep='\t', index=False, header=True, quoting=0)
+
+print(cell_name + ": finished.")
---
+++ 1_integration.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import os
+import sys
+
+cell_directory = sys.argv[1]
+script_directory = sys.argv[2]
+input_directory = sys.argv[3]
+prob_cutoff = float(sys.argv[4])
+
+cell_name = cell_directory.split('/')[-1]
+sample_name = input_directory.split('/')[-1]
+
+prediction_directory = script_directory + "/" + "ecDNA_prediction_" + sample_name
+
+output_directory = script_directory + "/" + "ecDNA_summary_" + sample_name
+
+# %%
+
+prediction_file_list = os.listdir(prediction_directory)
+
+cell_file_list = os.listdir(input_directory)
+cell_file_list = [file for file in cell_file_list if not file.lower().endswith('.ds_store')] # for macOS compatibility
+
+# %%
+
+a = pd.read_table(prediction_directory + "/" + prediction_file_list[0], header=0)
+coord = a.iloc[:, 0:3]
+
+# %%
+
+m_cnv = pd.DataFrame(0, index=coord.index, columns=prediction_file_list)
+m_ratio = m_cnv.copy()
+m_gini = m_cnv.copy()
+m_pred = m_cnv.copy()
+
+# %%
+
+# Loop through ann and read data from files
+for i, cell in enumerate(prediction_file_list):
+    res = pd.read_table(prediction_directory + "/" + cell, header=0)
+
+    m_cnv[cell] = res['CNV']
+    m_ratio[cell] = res['inter.intra.log2ratio']
+    m_gini[cell] = res['gini']
+    m_pred[cell] = res['pred']
+
+# %%
+
+
+# Concatenate output dataframes with 'out'
+final_cnv = pd.concat([coord, m_cnv], axis=1)
+final_ratio = pd.concat([coord, m_ratio], axis=1)
+final_gini = pd.concat([coord, m_gini], axis=1)
+final_pred = pd.concat([coord, m_pred], axis=1)
+
+# %%
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+# Write results to files
+final_cnv.to_csv(f'{output_directory}/{sample_name}_cnv.txt', sep='\t', index=False)
+final_ratio.to_csv(f'{output_directory}/{sample_name}_ratio.txt', sep='\t', index=False)
+final_gini.to_csv(f'{output_directory}/{sample_name}_gini.txt', sep='\t', index=False)
+final_pred.to_csv(f'{output_directory}/{sample_name}_pred.txt', sep='\t', index=False)
+
+print("All cells summarized.")
+
+# %%
+binary_pred = final_pred.iloc[:, 3:].copy()
+for i in range(len(binary_pred.columns)):
+    binary_pred[binary_pred.columns[i]] = (binary_pred[binary_pred.columns[i]] > prob_cutoff).astype(int)
+
+count = binary_pred.sum(axis=1)
+freq = count / len(binary_pred.columns)
+
+final_count_freq = pd.concat([coord, count, freq], axis=1)
+final_count_freq.columns = ['chr', 'start', 'end', 'count', 'freq']
+
+final_count_freq.to_csv(f'{output_directory}/{sample_name}_count_freq.txt', sep='\t', index=False)
+
+# %%
